f90fd4111c8a22e836903062838315b8cd2519d8
Kein Moon <successhelper2000@gmail.com>
2025-03-27 03:00:38 +0900

    fix : 버그수정 완료

--- bin/dart_debug_sample.dart
+++ bin/dart_debug_sample.dart
@@ -2,6 +2,8 @@ import 'package:dart_debug_sample/dart_debug_sample.dart';
 
 void main() {
   YukymController yukymController = YukymController();
-  print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
+
+  print('현재 시간: ${DateTime.now()}\n');
+  print(yukymController.getTyA() ); // 해당 월에 맞는 자시의 국 표시
   print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
 }
--- lib/dart_debug_sample.dart
+++ lib/dart_debug_sample.dart
@@ -3,7 +3,11 @@ import 'package:intl/intl.dart';
 class YukymController {
 
   // DateTime.parse(_userData.value!.selectDate)
-  String nowDate = DateFormat('yyyy-mm-dd').format(DateTime.now());
+  String nowDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+  // 디버깅 사항---------------------------------------------------
+  // 'yyyy-mm-dd' 여기에서 mm은 분(minute) 두자리를 나타낸다.
+  // MM 월(Month) 두자리로 변경해서 정확한 날짜를 가져올 수 있도록 수정
+  // -------------------------------------------------------------
 
   late String nowTime;
 
@@ -42,28 +46,35 @@ class YukymController {
     String result = timeDataOne.first.ty12;
 
     final nowTime = DateTime.now();
-    if (nowTime.hour >= 0 || nowTime.hour < 2) {
-      return timeDataOne.first.ty1;
-    } else if (nowTime.hour >= 4 || nowTime.hour < 6) {
-      return timeDataOne.first.ty2;
-    } else if (nowTime.hour >= 6 || nowTime.hour < 8) {
-      return timeDataOne.first.ty3;
-    } else if (nowTime.hour >= 8 || nowTime.hour < 10) {
-      return timeDataOne.first.ty4;
-    } else if (nowTime.hour >= 10 || nowTime.hour < 12) {
-      return timeDataOne.first.ty5;
-    } else if (nowTime.hour >= 12 || nowTime.hour < 14) {
-      return timeDataOne.first.ty6;
-    } else if (nowTime.hour >= 16 || nowTime.hour < 18) {
-      return timeDataOne.first.ty7;
-    } else if (nowTime.hour >= 18 || nowTime.hour < 20) {
-      return timeDataOne.first.ty8;
-    } else if (nowTime.hour >= 20 || nowTime.hour < 22) {
-      return timeDataOne.first.ty9;
-    } else if (nowTime.hour >= 22 || nowTime.hour < 24) {
-      return timeDataOne.first.ty10;
-    }
 
+    // 디버깅 사항---------------------------------------------------
+    // 24시간 설정이 제대로 되어 있지 않고, || 가 아닌 && 로 수정.
+    // -------------------------------------------------------------
+    if (nowTime.hour >= 0 && nowTime.hour < 2) {
+      return timeDataOne.first.ty1;
+    } else if (nowTime.hour >= 2 && nowTime.hour < 4) {
+      return timeDataOne.first.ty2;
+    } else if (nowTime.hour >= 4 && nowTime.hour < 6) {
+      return timeDataOne.first.ty3;
+    } else if (nowTime.hour >= 6 && nowTime.hour < 8) {
+      return timeDataOne.first.ty4;
+    } else if (nowTime.hour >= 8 && nowTime.hour < 10) {
+      return timeDataOne.first.ty5;
+    } else if (nowTime.hour >= 10 && nowTime.hour < 12) {
+      return timeDataOne.first.ty6;
+    } else if (nowTime.hour >= 12 && nowTime.hour < 14) {
+      return timeDataOne.first.ty7;
+    } else if (nowTime.hour >= 14 && nowTime.hour < 16) {
+      return timeDataOne.first.ty8;
+    } else if (nowTime.hour >= 16 && nowTime.hour < 18) {
+      return timeDataOne.first.ty9;
+    } else if (nowTime.hour >= 18 && nowTime.hour < 20) {
+      return timeDataOne.first.ty10;
+    } else if (nowTime.hour >= 20 && nowTime.hour < 22) {
+      return timeDataOne.first.ty11;
+    } else if (nowTime.hour >= 22 && nowTime.hour < 24) {
+      return timeDataOne.first.ty12;
+  }
     return result;
   }
 
